70735dd3c21ccea888e1eb058952bde711afa05e
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2019-04-03 20:05:35 +0200

    you can now login

--- script/login.js
+++ script/login.js
@@ -1,4 +1,16 @@
-let users = JSON.parse(localStorage.getItem('user'));
+'use strict'
+
+let config = {
+    apiKey: "AIzaSyAWkinQIM4C_2YHjUq1RYr2wk4q_pIX5HY",
+    authDomain: "project-c21db.firebaseapp.com",
+    databaseURL: "https://project-c21db.firebaseio.com",
+    projectId: "project-c21db",
+    storageBucket: "project-c21db.appspot.com",
+    messagingSenderId: "575431938944"
+};
+
+firebase.initializeApp(config);
+
 let email = document.getElementById('email');
 let emailText = document.getElementById('faultemail');
 let password = document.getElementById('password');
@@ -8,69 +20,72 @@ let activeUser, faultemail, faultpassword;
 let defaultColor = 'yellow';
 let errorColor = 'red';
 
-function check(){
-    button.addEventListener('click', submit);
-    if(typeof(localStorage) !== 'undefined'){
+let faultPassword = '';
+let faultEmail = '';
 
-        email.addEventListener('keyup', () => {
-            checkEmail(email.value, emailText);
-        });
-        password.addEventListener('keyup', () => {
-            checkPassword(password.value, passwordText);
-        });
-    }
+function check() {
+    button.addEventListener('click', submit);
+    email.addEventListener('keyup', ()=>{
+        email.style.borderBottomColor = defaultColor;
+        emailText.innerHTML = '';
+    });
+    password.addEventListener('keyup', ()=>{
+        password.style.borderBottomColor = defaultColor;
+        passwordText.innerHTML = '';
+    });
 }
-function submit(){
+
+function submit() {
     checkBlank(email.value, 'Email');
     checkBlank(password.value, 'Password');
     checkFaults();
 }
 
-function checkEmail(email, emailText){
-    if(users != null){
-        for(let user of users){
-            if(email === user.email){
-                faultEmail = '';
-                emailText.innerHTML = '';
-                document.getElementById('email').style.borderBottomColor = defaultColor;
-                activeUser = user;
-            }   
-            else{
-                faultEmail = 'There is no account using this email!';
-                emailText.innerHTML = 'There is no account using this email!';
-                document.getElementById('email').style.borderBottomColor = errorColor;
-            }
-        }
-    }
-    
-}
+// function checkEmail(email, emailText){
+//     if(users != null){
+//         for(let user of users){
+//             if(email === user.email){
+//                 faultEmail = '';
+//                 emailText.innerHTML = '';
+//                 document.getElementById('email').style.borderBottomColor = defaultColor;
+//                 activeUser = user;
+//             }   
+//             else{
+//                 faultEmail = 'There is no account using this email!';
+//                 emailText.innerHTML = 'There is no account using this email!';
+//                 document.getElementById('email').style.borderBottomColor = errorColor;
+//             }
+//         }
+//     }
 
-function checkPassword(password, passwordText){
-    if(activeUser === undefined){
-        faultPassword = 'There is no email selected!';
-        passwordText.innerHTML = 'There is no email selected!';
-        document.getElementById('password').style.borderBottomColor = errorColor;
-    }
-    else{
-        if(activeUser.password == password){
-            faultPassword = '';
-            passwordText.innerHTML = '';
-            document.getElementById('password').style.borderBottomColor = defaultColor;
-        }
-        else{
-            faultPassword = 'Password is incorrect!';
-            passwordText.innerHTML = 'Password is incorrect!';
-            document.getElementById('password').style.borderBottomColor = errorColor;
-        }
-    }
-}
+// }
 
-function checkBlank(field, fieldString){
+// function checkPassword(password, passwordText){
+//     if(activeUser === undefined){
+//         faultPassword = 'There is no email selected!';
+//         passwordText.innerHTML = 'There is no email selected!';
+//         document.getElementById('password').style.borderBottomColor = errorColor;
+//     }
+//     else{
+//         if(activeUser.password == password){
+//             faultPassword = '';
+//             passwordText.innerHTML = '';
+//             document.getElementById('password').style.borderBottomColor = defaultColor;
+//         }
+//         else{
+//             faultPassword = 'Password is incorrect!';
+//             passwordText.innerHTML = 'Password is incorrect!';
+//             document.getElementById('password').style.borderBottomColor = errorColor;
+//         }
+//     }
+// }
+
+function checkBlank(field, fieldString) {
     let patt = /./gm;
     let result = patt.test(field);
 
-    if(!result){
-        switch(fieldString){
+    if (!result) {
+        switch (fieldString) {
             case 'Email':
                 faultEmail = `${fieldString} can't be blank!`;
                 emailText.innerHTML = `${fieldString} can't be blank!`;
@@ -85,11 +100,29 @@ function checkBlank(field, fieldString){
     }
 }
 
-function checkFaults(){
-    if(faultPassword === '' && faultEmail == ''){
-        if(typeof(Storage) !== undefined){
-            localStorage.setItem('activeUser', JSON.stringify(activeUser));
-            window.open('../html/home.html', '_self')
-        }
+function checkFaults() {
+    if (faultPassword === '' && faultEmail == '') {
+        signInUser(document.getElementById('email').value, document.getElementById('password').value);
     }
+    faultpassword = '';
+    faultEmail = '';
 }
+
+function signInUser(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log('%c ' + errorMessage + ' ' + errorCode, 'color: #FF0000');
+        switch (errorCode) {
+            case 'auth/user-not-found':
+                document.getElementById('faultemail').innerHTML = 'Wrong email!';
+                email.style.borderBottomColor = errorColor;
+                break;
+            case 'auth/wrong-password':
+                document.getElementById('faultpassword').innerHTML = 'Wrong password!';
+                password.style.borderBottomColor = errorColor;
+                break;
+        }
+    });
+}
+
